73879719796d95ce6e22fcae39d5c87a360470ca
Marko Calvo-Cruz <markocalvocruz@gmail.com>
2020-04-11 22:57:53 -0500

    firebase config changes'

--- webrtc/client/test.js
+++ webrtc/client/test.js
@@ -1,44 +1,67 @@
 // Set the configuration for your app
 // TODO: Replace with your project's config object
-var config = {
-  apiKey: "AIzaSyDOnKy-7VXVobK80o7PDRD78yUAmsGm8TQ",
-  authDomain: "mapvideo-ddb83.firebaseapp.com",
-  databaseURL: "https://mapvideo-ddb83.firebaseio.com/",
-  storageBucket: "bucket.appspot.com"
-};
+
 console.log("Testing")
-firebase.initializeApp(config);
 // Get a reference to the database service
 var database = firebase.database();
 var rootRef = database.ref("rooms/r1")
-function getUserPositions() {
-    rootRef.on("value")
-      .then(function(snapshot) {
-          return getPositionsFromSnapshot(snapshot)
-})
+var USERID = rootRef.child("users").push().key
+// function getUserPositions() {
+//     rootRef.on("value")
+//       .then(function(snapshot) {
+//           return getPositionsFromSnapshot(snapshot)
+// })
+//
+// function moveUserLeft(user) {
+//   var user = {
+//     x: user.xCoord - 1,
+//     y: user.yCoord
+//   }
+//   updateUserPosition(user)
+// }
+// function moveUserRight(user) {
+//   var user = {
+//     x: user.xCoord + 1,
+//     y: user.yCoord
+//   }
+//   updateUserPosition(user)
+// }
+// function moveUserDown(user) {
+//   var user = {
+//     x: user.xCoord,
+//     y: user.yCoord + 1
+//   }
+//   updateUserPosition(user)
+// }
+// function moveUserUp(user) {
+//   var user = {
+//     x: user.xCoord,
+//     y: user.yCoord - 1
+//   }
+//   updateUserPosition(user)
+// }
+// function updateUserPosition(user) {
+//   var userRef = rootRef.child(`users/${USERID}`)
+//   userRef.set({
+//     x: user.xCoord,
+//     y: user.yCoord
+//   })
+// }
 
-
-function updateUserPosition(user) {
-  var userRef = rootRef.child(`users/${user.id}`)
-  userRef.set({
-    x: user.xCoord,
-    y: user.yCoord
-  })
-}
-function getPositionsFromSnapshot(snapshot) {
-  var positions = []
-  snapshot.forEach(function(userSnapshot) {
-      var userID = userSnapshot.key
-      var userData = userSnapshot.val()
-      var xCoord = userData.xCoord
-      var yCoord = userData.yCoord
-      var userData = {
-          userID: userID,
-          xCoord:  xCoord,
-          yCoord: yCoord
-      }
-      console.log(userData)
-      positions.append(userData)
-  })
-  return positons
-}
+// function getPositionsFromSnapshot(snapshot) {
+//   var positions = []
+//   // snapshot.forEach(function(userSnapshot) {
+//   //     var userID = userSnapshot.key
+//   //     var userData = userSnapshot.val()
+//   //     var xCoord = userData.xCoord
+//   //     var yCoord = userData.yCoord
+//   //     var userData = {
+//   //         userID: userID,
+//   //         xCoord:  xCoord,
+//   //         yCoord: yCoord
+//   //     }
+//   //     console.log(userData)
+//   //     positions.append(userData)
+//   // })
+//   return positons
+// }
